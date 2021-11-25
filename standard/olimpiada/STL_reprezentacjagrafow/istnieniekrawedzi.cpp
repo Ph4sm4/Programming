69635a4d240251103af97ca8e1fbe5f87fb73d3f
Ph4sm4 <78483172+Ph4sm4@users.noreply.github.com>
2021-11-25 23:51:04 +0100

    .

--- standard/olimpiada/STL_reprezentacjagrafow/istnieniekrawedzi.cpp
+++ standard/olimpiada/STL_reprezentacjagrafow/istnieniekrawedzi.cpp
@@ -2,7 +2,19 @@
 using namespace std;
 
 vector<int> tab[1000010];
-//56 przechodzi wszystkie testy ale na wiekszych obcina punkty
+//100
+int lb(int a, int val){
+    if(tab[a].size() == 0) return -1;
+    int l = 0, p = tab[a].size()-1, s;
+    while(l < p){
+        s = (l + p)/2;
+        if(tab[a][s] >= val){
+            p = s;
+        } else l = s + 1;
+    }
+    return (tab[a][l] == val? l : -1);
+}
+
 int main(){
     cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
     int n, m, a, b;
@@ -13,11 +25,14 @@ int main(){
         tab[a].push_back(b);
         tab[b].push_back(a);
     }
+    for(int i = 0; i < 1000010; i++){
+        sort(tab[i].begin(), tab[i].end());
+    }
     int q;
     cin>>q;
     for(int i = 0; i < q; i++){
         cin>>a>>b;
-        if(find(tab[a].begin(), tab[a].end(), b) != tab[a].end()) cout<<"TAK\n";
+        if(lb(a, b) != -1) cout<<"TAK\n";
         else cout<<"NIE\n";
     }
 
