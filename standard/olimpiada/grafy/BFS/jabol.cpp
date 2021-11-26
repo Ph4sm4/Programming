f5391cc98a6311a9a62ea9563d60d9b61df13d8c
Ph4sm4 <78483172+Ph4sm4@users.noreply.github.com>
2021-11-26 11:17:15 +0100

    .

---
+++ standard/olimpiada/grafy/BFS/jabol.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> tab[1000010];
+queue<int> q;
+int dist[1000010];
+
+int main(){
+    cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+    int n, k;
+    cin>>n>>k;
+    for(int i = 0; i < n - 1; i++){
+        int a, b;
+        cin>>a>>b;
+        tab[a].push_back(b);
+        tab[b].push_back(a);
+    }
+    vector<int> v(k);
+    for(int i = 0; i < k; i++) cin>>v[i];
+    int max_dis = 0;
+    for(auto el : v){
+        bool vis[1000010];
+        for(int i = 0; i < 1000010; i++) vis[i] = false;
+        dist[el] = 0;
+        vis[el] = true;
+        q.push(el);
+        while(!q.empty()){
+            int a = q.front();
+            q.pop();
+            for(int sasiad : tab[a]){
+                if(!vis[sasiad]){
+                    vis[sasiad] = true;
+                    dist[sasiad] = dist[a] + 1;
+                    q.push(sasiad);
+                }
+            }
+        }
+        for_each(v.begin(), v.end(), [&max_dis](int a){if(dist[a] > max_dis) max_dis = dist[a];});
+    }
+    cout<<max_dis;
+    
+
+    return 0;
+}
