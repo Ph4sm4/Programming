b827d53f3c7539c556e9388c4063c2f3cc38bf21
Ph4sm4 <78483172+Ph4sm4@users.noreply.github.com>
2022-04-14 22:25:55 +0200

    .

--- standard/admin/1.cpp
+++ standard/admin/1.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
+    cout.tie(0);
+    cin.tie(0);
+    ios_base::sync_with_stdio(0);
+    int tab[10];
+    for (int i = 0; i < 10; i++)
+    {
+        cin >> tab[i];
+        if (tab[i] % 2 == 0)
+        {
+            cout << tab[i] * 10 << endl;
+        }
+        else
+        {
+            cout << tab[i] << endl;
+        }
+    }
 
     return 0;
 }
