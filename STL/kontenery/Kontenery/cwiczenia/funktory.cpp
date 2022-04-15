2b2a9cde6b2fb2a869c1661390eba3296aea52ec
Ph4sm4 <78483172+Ph4sm4@users.noreply.github.com>
2022-04-15 17:15:02 +0200

    .

--- STL/kontenery/Kontenery/cwiczenia/funktory.cpp
+++ STL/kontenery/Kontenery/cwiczenia/funktory.cpp
@@ -2,19 +2,22 @@
 #include "printSTL.h"
 using namespace std;
 
-class Funktor{
-    public:
-        int pole;
-        int operator() (int a) const{
-            return a*a;
-        }
+class Funktor
+{
+public:
+    int pole;
+    int operator()(int a) const
+    {
+        return a * a;
+    }
 };
 
-int main(){
+int main()
+{
     Funktor f, f1;
-    cout<<f(10)<<endl;
-    cout<<f1(20)<<endl;
-    vector<int> v = {1,2,3,4,5,6};
+    cout << f(10) << endl;
+    cout << f1(20) << endl;
+    vector<int> v = {1, 2, 3, 4, 5, 6};
     vector<int> v2;
     transform(v.begin(), v.end(), back_inserter(v2), bind(multiplies<int>(), placeholders::_1, 10));
     printSTL(v2, " ");
@@ -23,14 +26,12 @@ int main(){
 
     v2.erase(
         remove_if(v2.begin(), v2.end(),
-            bind(logical_and<int>(),
-                bind(greater_equal<int>(), placeholders::_1, 40),
-                bind(less_equal<int>(), placeholders::_1, 99))
-            ),
+                  bind(logical_and<int>(),
+                       bind(greater_equal<int>(), placeholders::_1, 40),
+                       bind(less_equal<int>(), placeholders::_1, 99))),
         v2.end());
-    
-    printSTL(v2, " ");
 
+    printSTL(v2, " ");
 
     return 0;
 }
