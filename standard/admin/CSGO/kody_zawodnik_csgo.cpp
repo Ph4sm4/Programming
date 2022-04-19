4bfcb68ba54369501cd3c47e96d9b21ee9b299e7
Ph4sm4 <78483172+Ph4sm4@users.noreply.github.com>
2022-04-19 12:16:10 +0200

    .

---
+++ standard/admin/CSGO/kody_zawodnik_csgo.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int n = 1000;
+int chooseRandom()
+{
+    int type = rand() % 3;
+
+    if (!type)
+        return rand() % 10 + 48;
+
+    if (type == 1)
+        return rand() % 26 + 65;
+
+    return rand() % 26 + 97;
+}
+
+int main()
+{
+    srand(time(NULL));
+    vector<string> v;
+    // cyfry: 48 - 57
+    // duze litery: 65 - 90
+    // male litery: 97 - 122 wszystko wlacznie jest
+    fstream plik;
+    plik.open("kody_zawodnik_csgo.txt", ios::out);
+    string kod = "CSZ";
+    while (v.size() != n)
+    {
+        while (kod.length() != 6)
+        {
+            kod += char(chooseRandom());
+        }
+        if (!count(v.begin(), v.end(), kod))
+            v.push_back(kod);
+        kod = "CSZ";
+    }
+    copy(v.begin(), v.end(), ostream_iterator<string>(plik, "\n"));
+    plik.close();
+
+    return 0;
+}
---
+++ standard/admin/LOL/kody_kapitan_lol.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int n = 200;
+int chooseRandom()
+{
+    int type = rand() % 3;
+
+    if (!type)
+        return rand() % 10 + 48;
+
+    if (type == 1)
+        return rand() % 26 + 65;
+
+    return rand() % 26 + 97;
+}
+
+int main()
+{
+    srand(time(NULL));
+    vector<string> v;
+    // cyfry: 48 - 57
+    // duze litery: 65 - 90
+    // male litery: 97 - 122 wszystko wlacznie jest
+    fstream plik;
+    plik.open("kody_kapitan_lol.txt", ios::out);
+    string kod = "LOK";
+    while (v.size() != n)
+    {
+        while (kod.length() != 6)
+        {
+            kod += char(chooseRandom());
+        }
+        if (!count(v.begin(), v.end(), kod))
+            v.push_back(kod);
+        kod = "LOK";
+    }
+    copy(v.begin(), v.end(), ostream_iterator<string>(plik, "\n"));
+    plik.close();
+
+    return 0;
+}
