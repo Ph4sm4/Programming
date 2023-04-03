ec3f9d687637257d2f36c0f1115384195413fbc1
Ph4sm4 <78483172+Ph4sm4@users.noreply.github.com>
2023-04-03 17:59:52 +0200

    Update iteracja.cpp

--- standard/Przemek/iteracja.cpp
+++ standard/Przemek/iteracja.cpp
@@ -13,6 +13,7 @@ int main()
 
     // lub:
     // typ_zmiennej nazwa_zmiennej1, nazwa_zmiennej2;
+
     int liczba = 5 + 3;
     liczba = liczba + 7;
     liczba = liczba * 3;
@@ -26,8 +27,56 @@ int main()
         wynik = wynik + imie;
     }
 
-    cout << wynik << endl;
+    /* wytlumacz jaka jest roznica miedzy zapisem:
+    string imie = "Olaf";
+    cout << imie;
 
+    a
+
+    string imie = "Olaf";
+    cout << "imie";
+
+    =========================================================
+
+    ktory kod zadziala, a ktory nie :
+string imie = "Olaf";
+    1. cout << imie + "Dalach";
+
+    2.
+    string imie = "Olaf";
+    string nazwisko = "Dalach";
+    cout << imie + nazwisko;
+
+    =========================================================
+
+    co zostanie wypisane?
+    int liczba = 5.5;
+    cout << liczba;
+
+    float liczba2 = 2.123;
+    cout << liczba2;
+
+    =========================================================
+
+    co zostanie wypisane?
+    int liczba1 = 3;
+    int liczba2 = 4;
+    cout << liczba1 / liczba2;
+
+    int liczba3 = 4;
+    float liczba4 = 5;
+    cout << liczba3 / liczba4;
+
+
+    =========================================================
+
+    1. napisz petle, ktora do liczby 10 doda liczbe podana przez uzytkownika 7 razy
+    2. napisz petle, ktora do slowa "drzewo" dopisze "kasztanowe" 3 razy (wynik ma byc: drzewokasztanowekasztanowekasztanowe)
+    3. napisz petle, ktora sprawdzi w jaki sposob liczba 4 dzieli sie przez liczby 1,2,...,10 (zalezy nam na DOKLADNYM wyniku - z przecinkiem)
+    4. napisz petle, ktora przemnozy przez siebie liczbe 2, dziesiec razy
+    */
+
+    cout << wynik << endl;
     // int n;
     // cout << "Podaj n: ";
     // cin >> n;
@@ -51,7 +100,7 @@ int main()
 }
 
 // 1. Napisz program, który dla dowolnej liczby naturalnej, określi ilość cyfr tej liczby.
-// 2. Napisz program, który dla dwóch dowolnych liczb naturalnych a i b, podniesie ab.
+// 2. Napisz program, który dla dwóch dowolnych liczb naturalnych a i b, podniesie a do potegi b.
 
 int iloscCyfr(int x)
 {
