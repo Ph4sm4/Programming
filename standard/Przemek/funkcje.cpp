cf9fd558298b0c62b6c9f40843ec95ba488ec9a7
Ph4sm4 <78483172+Ph4sm4@users.noreply.github.com>
2023-04-26 17:14:59 +0200

    .

--- standard/Przemek/funkcje.cpp
+++ standard/Przemek/funkcje.cpp
@@ -1,29 +1,48 @@
 #include <iostream>
 using namespace std;
 
+int iloraz(int a, int b)
+{
+    if (b == 0)
+    {
+        cout << "nie dzielimy przez ";
+        return 0;
+    }
+    // ...
+    return a / b;
+}
+
 int main()
 {
-
-    int wiek;
-    cin >> wiek;
-    switch (wiek)
-    {
-    case 10:
-    {
-        cout << "dziesieciolatek";
-        break;
-    }
-    case 20:
-    {
-        cout << "dwudziestwolatek";
-        break;
-    }
-    case 30:
-    {
-        cout << "trzydziestolatek";
-        break;
-    }
-    }
+    cout << iloraz(10, 0);
+    // int wiek;
+    // cin >> wiek;
+    // switch (wiek)
+    // {
+    // case 10:
+    // {
+    //     cout << "dziesieciolatek";
+    //     break;
+    // }
+    // case 20:
+    // {
+    //     cout << "dwudziestwolatek";
+    //     break;
+    // }
+    // case 30:
+    // {
+    //     cout << "trzydziestolatek";
+    //     break;
+    // }
+    // case 40:
+    // {
+    //     cout << 'h';
+    //     break;
+    // }
+    // default:
+    // {
+    // }
+    // }
 
     return 0;
 }
