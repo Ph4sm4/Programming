eb494ec569bb7209aa409db4a42a8414a5f62d41
Olaf Dalach <dalach.olaf@gmail.com>
2023-06-02 23:46:53 +0200

    .

---
+++ standard/Przemek/while.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    // for (int i = 0; i < 10; i++)
+    // {
+    //     cout << i;
+    // }
+
+    int it = 30;
+    string napis = "drwal";
+
+    while (it < 10)
+    {
+        cout << napis;
+        it++;
+    }
+
+    return 0;
+}
